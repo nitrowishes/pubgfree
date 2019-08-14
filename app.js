339a853cd804b70bc547acc84070d78c8e4e6d37
nitrowishes <52922548+nitrowishes@users.noreply.github.com>
2019-08-14 21:07:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-/*
+
 if(window.location.hostname != "nitrowishes.ml"){
     window.location.href="http://www.moviesfun.in/";
     throw new Error("FUCK YOUR MOTHER ASSHOLE");
   }
-*/
 
 var myapp = new Vue({
     el: "#app",
